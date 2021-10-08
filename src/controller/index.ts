5ebe4981370070d817d978d740b532a137c4a8b2
Jino <jino.liu@klook.com>
2021-10-08 15:59:54 +0800

    init

---
+++ src/controller/index.ts
@@ -0,0 +1,43 @@
+import { Readable } from 'stream'
+import { Controller, Get, Provide, Inject } from '@midwayjs/decorator'
+import { Context } from 'egg'
+import { render } from 'ssr-core-vue'
+import { IApiService, IApiDetailService } from '../interface'
+
+interface IEggContext extends Context {
+  apiService: IApiService
+  apiDeatilservice: IApiDetailService
+}
+
+@Provide()
+@Controller('/')
+export class Index {
+  @Inject()
+  ctx: IEggContext
+
+  @Inject('ApiService')
+  apiService: IApiService
+
+  @Inject('ApiDetailService')
+  apiDeatilservice: IApiDetailService
+
+  @Get('/')
+  @Get('/detail/:id')
+  async handler (): Promise<void> {
+    try {
+      this.ctx.apiService = this.apiService
+      this.ctx.apiDeatilservice = this.apiDeatilservice
+
+      console.log('app.config.keys', this.ctx.app.config.keys)
+      console.log('app.config.test', this.ctx.app.config.test)
+
+      const stream = await render<Readable>(this.ctx, {
+        stream: true
+      })
+      this.ctx.body = stream
+    } catch (error) {
+      console.log(error)
+      this.ctx.body = error
+    }
+  }
+}
---
+++ typings/app/index.d.ts
@@ -0,0 +1,7 @@
+// This file is created by egg-ts-helper
+// Do not modify this file!!!!!!!!!
+import 'egg';
+import '@midwayjs/web';
+import 'egg-framework-test';
+export * from 'egg';
+export as namespace Egg;
